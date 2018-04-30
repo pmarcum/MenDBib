403d450b800d9ee95f6ae4090083fd80f39c56fc
Pamela Marcum <mucramap@yahoo.com>
2018-04-30 16:24:19 -0700

    Update stopWords.js

--- stopWords.js
+++ stopWords.js
@@ -5,13 +5,13 @@ var stopWords =
             'another|annals|any|anybody|anyone|anything|anywhere|appendix|appendices|' +
             'appreciable|appreciate|appreciated|appreciates|appreciating|appreciably|' +
             'approached|approaches|approaching|approach|appropriate|approximate|approximately|approximated|' +
-            'approximation|approximations|applied|apply|applying|arbitrary|arbitrarily|' +
+            'approximation|approximations|applied|apply|applying|april|apr|arbitrary|arbitrarily|' +
             'are|arent|arcsecond|arcseconds|arcsec|arcsecs|arcminute|arcminutes|arcmin|arcmins|argue|' +
             'argues|argued|arguably|arise|arises|arose|arisen|arising|arrow|arrows|article|articles|' +
             'as|asc|ascension|aside|ask|asked|asking|asks|assess|assesses|assessed|assessing|' +
             'assure|assures|assuring|at|attain|attains|attained|attaining|attempt|attempts|attempted|' +
-            'attempting|attributed|attributing|attribution|attributions|award|awards|awarded|awarding|author|authors|' +
-            'average|away|automatically|' +
+            'attempting|attributed|attributing|attribution|attributions|august|aug|award|awards|' +
+            'awarded|awarding|author|authors|average|away|automatically|' +
         'backed|backing|backs|bad|barely|based|basic|basically|be|became|because|become|becomes|' +
             'been|before|began|begin|begins|being|beings|believed|believe|best|better|bigger|bold|boldly|boldness|boldface|' +
             'boldfaced|bookkeeping|bring|brings|bringing|brought|broadly|but|by|' +
@@ -25,7 +25,7 @@ var stopWords =
             'consists|consisting|consistency|convenient|convenience|conveniences|conveniently|' +
             'convince|convinced|convinces|convincing|convincingly|corporated|corporation|corporations|' +
             'corresponding|could|council|councils|counsel|counsels|counseling|currently|' +
-        'dash|dashed|dd|debate|debates|debating|debated|dec|decide|decides|decided|deciding|' +
+        'dash|dashed|dd|debate|debates|debating|debated|december|dec|decide|decides|decided|deciding|' +
             'decision|decisions|declare|delared|declares|declaring|declaration|declarations|' +
             'declination|declinations|dec|decl|deliberate|deliberately|demand|demands|demanded|' +
             'demanding|demonstrate|demonstrates|demonstrated|demonstrating|demonstration|department|' +
@@ -36,13 +36,13 @@ var stopWords =
             'discussing|discusses|distain|distains|do|does|doesnt|done|dont|dot|dots|dotted|' +
             'download|downloads|downloading|downloaded|dozen|dozens|during|' +
             'dramatic|dramatically|draw|drawn|drew|draws|drawing|due|' +
-        'each|easy|easier|easily|edition|eg|eight|either|else|elsewhere|employ|employs|employed|employing|' +
+        'each|easy|easier|easily|edition|eg|e\.g\.|eight|either|else|elsewhere|employ|employs|employed|employing|' +
             'enable|enabling|enabled|enables|endure|endures|enduring|endured|enlighten|enlightened|' +
             'enough|ensure|enter|enters|entry|entered|entering|entrance|especially|esp|et|etc|' +
             'eventual|eventually|ever|everybody|everyone|everything|evidently|excellence|excellent|' +
             'express|expresses|expressed|expressing|expression|expressions|expressive|' +
             'extravagant|' +
-        'fact|facts|fair|fairly|faired|famous|famously|feel|feels|fellowship|fellowships|felt|' +
+        'fact|facts|fair|fairly|faired|famous|famously|february|feb|feel|feels|fellowship|fellowships|felt|' +
             'figure|figures|finally|financial|financially|finances|financed|financing|firstly|' +
             'finish|finishes|finished|finishing|for|forth|forthcoming|fortuitous|fortuitously|foundation|' +
             'foundations|four|fifth|five|from|fruit|fruits|fruitful|fulfillment|fund|funded|funds|funding|' +
@@ -55,7 +55,8 @@ var stopWords =
         'had|happen|happens|happened|happening|happy|happily|has|hasnt|hasten|hastens|hastening|' +
             'have|having|he|help|helps|helped|helping|helpful|helpfully|henceforth|her|here|herself|hence|' +
             'him|himself|hint|hints|hinted|hinting|his|hour|hours|hr|hrs|how|however|http|huge|hz|' +
-        'i|ie|if|illustrate|illustrated|illustrating|illustrates|illustrative|imagine|imagines|imagining|imagined|imagination|' +
+        'i|ie|i\.e\.|if|illustrate|illustrated|illustrating|illustrates|illustrative|imagine|imagines|' +
+            'imagining|imagined|imagination|' +
             'immediate|immediately|impartial|impartially|implement|implemented|implements|' +
             'implementing|implementation|important|importance|impose|imposed|imposing|imposes|' +
             'impossible|impossibility|impossibilities|impressive|impress|impresses|impressed|' +
@@ -69,13 +70,13 @@ var stopWords =
             'intuitive|intuitively|intuit|intuits|invaluable|investigate|investigates|investigated|' +
             'investigating|investigation|investigator|investigators|invite|invitation|invites|invited|' +
             'inviting|invitations|invitational|is|issued|issuing|it|its|itself|' +
-        'jansky|janskies|journal|journals|just|justice|justification|' +
+        'jansky|janskies|january|jan|journal|journals|june|jun|july|jul|just|justice|justification|' +
             'justify|justified|justifies|justifying|jy|' +
         'key|kind|kindly|kinds|khz|km|km2|km3|kpc|kpc2|kpc3|' +
         'latter|later|latest|less|let|lets|like|likes|liked|liking|likely|likewise|linger|lingers|' +
             'lingered|lingering|list|lists|listed|listing|listings|litany|look|looked|looks|looking|' +
         'm|m2|m3|mm|mm2|mm3|made|main|make|making|mainly|maintain|maintains|maintained|maintaining|' +
-            'maintenance|manage|manages|managed|managing|many|manuscript|manuscripts|marginal|' +
+            'maintenance|manage|manages|managed|managing|many|manuscript|manuscripts|march|mar|marginal|' +
             'marked|marking|matters|materially|mattered|mattering|may|me|meaningful|meaningfully|' +
             'megaparsec|megaparsecs|mental|mentally|mention|mentions|mentioned|mentioning|mere|merely|' +
             'meter|meters|metre|metres|millijansky|millijanskys|mhz|mjy|middle|might|minute|minutes|' +
@@ -83,9 +84,10 @@ var stopWords =
             'motivates|motivating|motivated|mpc|mpc2|mpc3|much|must|my|myself|' +
         'national|nationally|nearly|necessarily|necessity|need|needed|needing|needs|neglect|' +
             'neither|nevertheless|news|next|nice|nicely|nine|nobody|noone|none|nor|not|notable|notably|' +
-            'note|notes|noted|noting|nothing|notwithstanding|notices|now|nowhere|number|numbers|numerously|' +
+            'note|notes|noted|noting|nothing|notwithstanding|notices|november|nov|now|' +
+            'nowhere|number|numbers|numerously|' +
         'obey|obeys|obeyed|obeying|object|objects|obtain|obtains|obtained|obtaining|obviously|occasion|' +
-            'occasional|occasions|occasionally|oclock|of|offer|offers|offered|offering|on|online|or|other|' +
+            'occasional|occasions|occasionally|oclock|october|oct|of|offer|offers|offered|offering|on|online|or|other|' +
             'others|otherwise|our|ours|out|outlandish|owe|owing|owes|owed|' +
         'page|pages|paginate|paginated|paginates|pagination|paper|papers|parsec|parsecs|parenthesis|participate|' +
             'participated|participates|participating|participant|participants|parting|partly|particular|' +
@@ -108,7 +110,7 @@ var stopWords =
             'result|results|reveal|reveals|revealing|revealed|review|reviews|reviewed|reviewing|richer|rigor|' +
             'rigorous|rigorously|room|roughly|routinely|' +
         's|ss|sabbatical|safely|said|saw|say|says|sec|secs|second|secondly|seconds|see|seek|seeks|seeking|seem|seemed|' +
-            'seeming|seemingly|seems|sees|serious|seriously|session|sessions|seven|six|shaded|shall|she|shortly|should|' +
+            'seeming|seemingly|seems|sees|september|sept|sep|serious|seriously|session|sessions|seven|six|shaded|shall|she|shortly|should|' +
             'show|showed|showing|shown|shows|society|societies|some|somehow|somewhat|sometime|sometimes|somewhere|someone|something|' +
             'soon|sooner|soonest|sought|speak|speaks|speaking|speaker|speakers|speculate|speculates|speculated|speculating|' +
             'spoke|spoken|statement|statements|states|stating|stated|study|studies|studied|studying|student|students|' +
